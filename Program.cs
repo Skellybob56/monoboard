2fd414751552ad73d563d177636768f733c1a2e4
skellybob56 <lucas.chowney@outlook.com>
2026-03-22 08:44:26 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace Monoboard;
+
+internal static class Program
+{
+    static readonly OutputDevice output = OutputDevice.GetByName("monoboard");
+
+    static void MidiUpdate()
+    {
+        if (IsKeyPressed(KeyboardKey.J)) { output.SendEvent(new NoteOnEvent((SevenBitNumber)60, (SevenBitNumber)100)); }
+        if (IsKeyReleased(KeyboardKey.J)) { output.SendEvent(new NoteOffEvent((SevenBitNumber)60, (SevenBitNumber)0)); }
+    }
+
+    // STAThread is required if you deploy using NativeAOT on Windows - See https://github.com/raylib-cs/raylib-cs/issues/301
+    [System.STAThread]
+    public static void Main()
+    {
+        InitWindow(800, 480, "Hello World");
+        
+        while (!WindowShouldClose())
+        {
+            MidiUpdate();
+
+            BeginDrawing();
+            ClearBackground(Color.White);
+
+            DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            EndDrawing();
+        }
+
+        CloseWindow();
+        output.Dispose();
+    }
+}
