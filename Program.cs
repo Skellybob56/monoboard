9e5aa6f1dbf9237e9ba48dc0ac208d1b0d4156b1
skellybob56 <lucas.chowney@outlook.com>
2026-03-24 11:19:25 +0000

    Add framerate limitations and stop rendering identical frames

--- Program.cs
+++ Program.cs
@@ -5,29 +5,47 @@ namespace Monoboard;
 
 internal static class Program
 {
+    const int desiredFramerate = 250;
+    const double desiredFrametime = 1d/desiredFramerate;
+
+    static void Render()
+    {
+        BeginDrawing();
+        ClearBackground(Color.Black);
+        DrawTextEx(GetFontDefault(), "Monoboard", new(12, 12), 20, 12, Color.White);
+        EndDrawing();
+    }
+
     // STAThread is required if you deploy using NativeAOT on Windows - See https://github.com/raylib-cs/raylib-cs/issues/301
     [System.STAThread]
     public static void Main()
     {
-        InitWindow(200, 60, "Monoboard");
+        InitWindow(220, 40, "Monoboard");
         Font jetBrainsMonoFont = LoadFont("assets/JetBrainsMono-Regular.ttf");
 
         Controller controller = Controller.Create();
         MidiManager midiManager = MidiManager.Create();
         Instrument instrument = Instrument.Create(midiManager);
 
+        // populate the screen buffer
+        Render();
+
         while (!WindowShouldClose())
         {
+            double frameStartTime = GetTime();
             Keymap latestKeymap = controller.GetKeymap();
             instrument.Update(latestKeymap, GetTime());
 
-            BeginDrawing();
-            ClearBackground(Color.Black);
-
-            DrawFPS(8, 8);
-            DrawTextEx(GetFontDefault(), "Monoboard", new(16, 32), 20, 10, Color.White);
-
-            EndDrawing();
+            bool renderNeeded = false; // replace when dynamic picture elements are added
+            if (renderNeeded)
+            { Render(); }
+            else
+            {
+                double timeTillNextFrame = desiredFrametime - (GetTime() - frameStartTime);
+                if (timeTillNextFrame < 0d) { continue; }
+                WaitTime(timeTillNextFrame);
+                PollInputEvents();
+            }
         }
 
         CloseWindow();
