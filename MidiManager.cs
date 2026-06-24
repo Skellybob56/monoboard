4bbb92a9b2c5a07e563e06e07d8e3683d7163781
skellybob56 <lucas.chowney@outlook.com>
2026-06-24 15:55:20 +0100

    Simplify MidiManager note event code

--- MidiManager.cs
+++ MidiManager.cs
@@ -20,45 +20,35 @@ class MidiManager : Singleton<MidiManager>
 	public void NoteEvent((int octave, int note)? noteData)
 	{
 		if (noteData is null)
-		{ NoteEventDirect(null); return; } // null noteData: treat as a rest
+		{ NoteOffEvent(); return; } // null noteData: treat as a rest
 
 		int newNoteInt = noteData.Value.octave * 12 + noteData.Value.note;
 		if (newNoteInt < 0x00 || newNoteInt > 0x7f)
-		{ NoteEventDirect(null); return; } // note out of range: treat as a rest
+		{ NoteOffEvent(); return; } // note out of range: treat as a rest
 
-		NoteEventDirect((byte)newNoteInt);
+		NoteOnEvent((byte)newNoteInt);
 	}
 
-	void NoteEventDirect(byte? newNote)
+	void NoteOnEvent(byte newNote)
 	{
-		if (newNote is null)
-		{
-			// note off
-			if (playingNote is null)
-			{ return; } // attempted to stop playing a note while no note was playing
-			NoteOffEvent(playingNote.Value);
-			playingNote = null;
-			return;
-		}
-
 		if (playingNote == newNote) { return; } // note is already playing
 
 		// note on
 		if (playingNote is not null)
-		{ NoteOffEvent(playingNote.Value); }
+		{ NoteOffEvent(); }
 
 		playingNote = newNote;
-		NoteOnEvent(playingNote.Value, noteVelocity);
+		// todo: ensure safety when note or velocity are above 0x7F
+		TEVirtualMidi.virtualMIDISendData(MidiPortHandler.LpvmMidiPort, [0x90, playingNote.Value, noteVelocity], 3);
 	}
 
-	// todo: ensure safety when note or velocity are above 0x7F
-	void NoteOnEvent(byte note, byte velocity)
+	void NoteOffEvent()
 	{
-		TEVirtualMidi.virtualMIDISendData(MidiPortHandler.LpvmMidiPort, [0x90, note, velocity], 3);
-	}
-	void NoteOffEvent(byte note)
-	{
-		TEVirtualMidi.virtualMIDISendData(MidiPortHandler.LpvmMidiPort, [0x80, note, 0x00], 3);
+		if (playingNote is null)
+		{ return; } // attempted to stop playing a note while no note was playing
+
+		TEVirtualMidi.virtualMIDISendData(MidiPortHandler.LpvmMidiPort, [0x80, playingNote.Value, 0x00], 3);
+		playingNote = null;
 	}
 
 	protected override void Dispose()
