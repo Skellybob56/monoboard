a4bc61bad41dcef5db06fae36b425cea357b60d4
skellybob56 <lucas.chowney@outlook.com>
2026-07-27 22:33:57 +0100

    Add per-glyph hardcoded offsets for centering

--- Renderer.cs
+++ Renderer.cs
@@ -28,30 +28,60 @@ class Renderer : Singleton<Renderer>
 		ClearBackground(Color.Black);
 		DrawTextEx(Program.font, "MONOBOARD", new(15, 12), Program.fontSize, 30, Color.White);
 
-		DrawKey(keySet1Start + 0*keyBoxJump, 72, 'A', Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.A));
-		DrawKey(keySet1Start + 1*keyBoxJump, 72, 'S', Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.S));
-		DrawKey(keySet1Start + 2*keyBoxJump, 72, 'D', Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.D));
-		DrawKey(keySet1Start + 3*keyBoxJump, 72, 'F', Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.F));
+		DrawKey(keySet1Start + 0*keyBoxJump, 72, Glyph.A, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.A));
+		DrawKey(keySet1Start + 1*keyBoxJump, 72, Glyph.S, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.S));
+		DrawKey(keySet1Start + 2*keyBoxJump, 72, Glyph.D, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.D));
+		DrawKey(keySet1Start + 3*keyBoxJump, 72, Glyph.F, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.F));
 
-		DrawKey(keySet2Start + 0*keyBoxJump, 72, 'J', Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.Up));
-		DrawKey(keySet2Start + 1*keyBoxJump, 72, 'K', Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.Down));
-		DrawKey(keySet2Start + 2*keyBoxJump, 72, 'L', Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.Sharp));
-		DrawKey(keySet2Start + 3*keyBoxJump, 72, ';', false);
+		DrawKey(keySet2Start + 0*keyBoxJump, 72, Glyph.J, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.Up));
+		DrawKey(keySet2Start + 1*keyBoxJump, 72, Glyph.K, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.Down));
+		DrawKey(keySet2Start + 2*keyBoxJump, 72, Glyph.L, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.Sharp));
+		DrawKey(keySet2Start + 3*keyBoxJump, 72, Glyph.Semicolon, false);
 
 		DrawSpace(keySet1Start, 132, Controller.CurrentKeymap.HasFlag(KeymapUtil.Keymap.ApplyOctave));
 	}
 
-	void DrawKey(int x, int y, char glyph, bool pressed)
+	void DrawKey(int x, int y, Glyph glyph, bool pressed)
 	{
 		int yOffset = pressed? keyPressedSink : 0;
 		DrawRectangleLines(x, y + yOffset, keyBoxWidth, keyBoxHeight, pressed? keyPressedColor : keyColor);
-		DrawCentredGlyph(new (x + keyBoxWidth/2, y + keyBoxHeight/2 + yOffset), glyph, pressed? keyPressedColor : keyColor);
+		DrawGlyph(x, y + yOffset, glyph, pressed? keyPressedColor : keyColor);
 	}
 
-	void DrawCentredGlyph(Vector2 centre, char glyph, Color color)
+	enum Glyph : byte
 	{
-		Vector2 glyphSize = MeasureTextEx(Program.font, new string(glyph, 1), Program.fontSize, 0);
-		DrawTextCodepoint(Program.font, glyph, centre - Vector2.Round(glyphSize/2f), Program.fontSize, color);
+		A, S, D, F,
+		J, K, L, Semicolon
+	}
+
+	static char GlyphToChar(Glyph glyph)
+	{
+		return glyph switch
+		{
+			Glyph.A => 'A', Glyph.S => 'S', Glyph.D => 'D', Glyph.F => 'F',
+			Glyph.J => 'J', Glyph.K => 'K', Glyph.L => 'L', Glyph.Semicolon => ';',
+			_ => throw new ArgumentException("Glyph enum value not recognized.", nameof(glyph))
+		};
+	}
+
+	static void DrawGlyph(int x, int y, Glyph glyph, Color color)
+	{
+		char character = GlyphToChar(glyph);
+
+		Vector2 offset = glyph switch
+		{
+			Glyph.A => new(8, 2),
+			Glyph.S => new(11, 2),
+			Glyph.D => new(7, 2),
+			Glyph.F => new(10, 2),
+			Glyph.J => new(18, -1),
+			Glyph.K => new(8, 2),
+			Glyph.L => new(9, 2),
+			Glyph.Semicolon => new(18, -2),
+			_ => throw new ArgumentException("Glyph enum value not recognized.", nameof(glyph))
+		};
+
+		DrawTextCodepoint(Program.font, character, new Vector2(x, y) + offset, Program.fontSize, color);
 	}
 
 	void DrawSpace(int x, int y, bool pressed)
