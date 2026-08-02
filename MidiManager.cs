7e166a0fdff1d92ceea0c43771ffb08e25c7a3b1
skellybob56 <lucas.chowney@outlook.com>
2026-08-02 11:26:18 +0100

    Fix bug in NoteOnEvent and return a bool from NoteEvent in case of handled failure

--- MidiManager.cs
+++ MidiManager.cs
@@ -19,23 +19,25 @@ class MidiManager : Singleton<MidiManager>
 		MidiPortHandler.Initialize(midiDeviceName);
 	}
 
-	public void NoteEvent((int octave, int note)? noteData)
+	// returns false when the note it out of midi range
+	public bool NoteEvent((int octave, int note)? noteData)
 	{
 		Program.ScheduleGraphicalUpdate(); // to update currently playing note display
 
 		if (noteData is null)
-		{ NoteOffEvent(); return; } // null noteData: treat as a rest
+		{ NoteOffEvent(); return true; } // null noteData is a rest
 
 		int newNoteInt = noteData.Value.octave * 12 + noteData.Value.note;
 		if (newNoteInt < 0x00 || newNoteInt > 0x7f)
-		{ NoteOffEvent(); return; } // note out of range: treat as a rest
+		{ NoteOffEvent(); return false; } // note out of range: treat as a rest and fail
 
 		NoteOnEvent((byte)newNoteInt);
+		return true;
 	}
 
 	void NoteOnEvent(byte newNote)
 	{
-		Debug.Assert(newNote < 0x7F);
+		Debug.Assert(newNote <= 0x7F);
 
 		if (PlayingNote == newNote) { return; } // note is already playing
 
